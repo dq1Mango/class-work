fbcb27bf0a24ac14597a2f520a9146200ab16295
Thomas Ranney <thomas@ranney.com>
2026-03-24 16:58:51 -0400

    started the bunnies and foxes project

---
+++ bunnies/main.go
@@ -0,0 +1,819 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"math/rand"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/AlexEidt/Vidio"
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/montanaflynn/stats"
+	// "slices"
+)
+
+type SiteState int
+
+// possible states of sites
+const (
+	Empty SiteState = iota
+	Bunny
+	Fox
+	// Visited
+	// Immune
+)
+
+const (
+	ATTEMPTS = 1
+	SIZE     = 101
+	TICKS    = 1e3
+
+	ALPHA = 0.8
+	BETA  = 0.1
+
+	RECORD   = true
+	FPS      = 15
+	VID_TIME = 10 // in seconds
+)
+
+var StateColor = map[SiteState]color.NRGBA{
+	Empty: {
+		R: 0,
+		G: 0,
+		B: 0,
+		A: 255,
+	},
+	Bunny: {
+		R: 0,
+		G: 0,
+		B: 255,
+		A: 255,
+	},
+	Fox: {
+		R: 255,
+		G: 0,
+		B: 0,
+		A: 255,
+	},
+}
+
+// representing a cartesian point on the grid
+type Point struct {
+	x int
+	y int
+}
+
+var CARDINALS = []Point{
+	{x: 0, y: -1},
+	{x: 0, y: 1},
+	{x: 1, y: 0},
+	{x: -1, y: 0},
+}
+
+func mid_point() Point {
+	// mid := (size - 1) / 2
+
+	return Point{x: 0, y: 0}
+}
+
+func real_mid_point(size int) Point {
+
+	mid := (size - 1) / 2
+
+	return Point{x: mid, y: mid}
+}
+
+func add_points(p1, p2 Point) Point {
+	return Point{x: p1.x + p2.x, y: p1.y + p2.y}
+}
+
+// func (p *Point) add(p2 Point) {
+// 	p.x += p2.x
+// 	p.y += p2.y
+// }
+
+// func (p *Point) flip() {
+// 	p.x *= -1
+// 	p.y *= -1
+// }
+
+func abs(x int) int {
+	return max(x, x*-1)
+}
+
+func (p *Point) radius() int {
+	return max(abs(p.x), abs(p.y))
+}
+
+// abstraction of a 2d grid such that sites can be acsessd with cartesian coordinates
+type Grid [][]SiteState
+
+func (g Grid) raw_index(point Point) *SiteState {
+	return &g[point.y][point.x]
+}
+
+func (g Grid) index(point Point) *SiteState {
+	real_point := add_points(point, real_mid_point(len(g)))
+
+	return &g[real_point.y][real_point.x]
+}
+
+func (g Grid) is_valid_point(point Point) bool {
+
+	radius := len(g) / 2
+
+	if point.x >= -radius && point.x <= radius && point.y >= -radius && point.y <= radius {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (g Grid) clone() Grid {
+
+	cloned := make(Grid, 0, len(g))
+
+	for _, row := range g {
+		new_row := make([]SiteState, 0, len(g))
+		for _, value := range row {
+
+			new_row = append(new_row, SiteState(value))
+		}
+		cloned = append(cloned, new_row)
+	}
+
+	return cloned
+}
+
+// model holds the different parametes that define the simulation
+// as well as the grids themselves
+type Model struct {
+	grid    Grid
+	grids   []Grid
+	size    int
+	time    int
+	ticks   int
+	bunnies []Point
+	foxes   []Point
+	frames  int
+}
+
+func (m *Model) index(point Point) *SiteState {
+	return m.grid.index(point)
+}
+
+// generates an empty grid of dimensions *size*
+func gen_grid(size int) Grid {
+
+	if size%2 == 0 {
+		panic("grid size must be odd you doofus")
+	}
+
+	grid := make(Grid, size)
+
+	for row := range grid {
+		grid[row] = make([]SiteState, size)
+	}
+
+	return grid
+}
+
+func round(x float64) int {
+	return int(math.Round(x))
+}
+
+func clear_screen() {
+	print("\u001b[2J")
+}
+
+func clear_line() {
+	print("\u001b[2K")
+	print("\r")
+}
+
+func (g Grid) draw_circle(center Point, radius int, value SiteState) {
+
+	circleEquation := func(x float64) float64 {
+		return math.Sqrt(float64(radius*radius) - x*x)
+	}
+
+	for x := -radius; x <= radius; x++ {
+		height := round(circleEquation(float64(x)))
+
+		for y := -height; y <= height; y++ {
+			point := add_points(Point{x: x, y: y}, center)
+			// fmt.Println(point)
+			*g.index(point) = value
+		}
+	}
+}
+
+func init_model(
+	size int,
+	ticks int,
+	frames int,
+	r *rand.Rand,
+) Model {
+
+	if size%2 == 0 {
+		panic("grid size must be odd you doofus")
+	}
+
+	// grid_type := "normal"
+	// grid_type := "yinyang"
+
+	// wether to use the yinyang pattern or not
+	// (this should probably be configurable from cmdline flags)
+	yinyang := false
+
+	var grid Grid
+	if !yinyang {
+		grid = gen_grid(size)
+
+		cutoff := 0.5
+
+		// sets each site to a random state
+		for i := range grid {
+			for j := range grid {
+				if r.Float64() < cutoff {
+					grid[i][j] = Bunny
+				} else {
+					grid[i][j] = Fox
+				}
+			}
+		}
+	}
+	// pretty_picture(table, "yinyang", true)
+	// panic("done")
+
+	model := Model{
+		grid:    grid,
+		grids:   []Grid{grid.clone()},
+		size:    size,
+		time:    1,
+		ticks:   ticks,
+		bunnies: make([]Point, 0),
+		foxes:   make([]Point, 0),
+		frames:  frames,
+	}
+
+	return model
+
+}
+
+func (m *Model) randomPoint(r *rand.Rand) Point {
+	radius := m.size / 2
+
+	return Point{x: r.Intn(m.size) - radius, y: r.Intn(m.size) - radius}
+}
+
+func (m *Model) tick(r *rand.Rand) {
+
+	var selected Point
+	selected = m.randomPoint(r)
+
+}
+
+// runs one *model* to completion
+func (m *Model) run_trial(r *rand.Rand) Data {
+
+	// precision := m.ticks / 100
+	// progress := -1
+
+	data := make(Data, 0, m.frames)
+	interval := m.ticks / m.frames
+
+	// run the model for the specified number of ticks (m.ticks)
+	for m.time < m.ticks {
+
+		// currentProgress := m.time / precision
+		// if currentProgress > progress {
+		// 	progress = currentProgress
+		// 	clear_line()
+		// 	fmt.Printf("This much done: %d", progress)
+		// }
+
+		if m.time%interval == 0 {
+
+			if RECORD {
+				clone := m.grid.clone()
+				m.grids = append(m.grids, clone)
+			}
+
+			// if realEnthalpy < 800 {
+			// 	fmt.Println(realEnthalpy)
+			// }
+
+			data = append(data, DataPoint{Time: m.time, Bunnies: realEnthalpy})
+			// almostEnthalpy := m.ApproxEnthalpy(r)
+			//
+			// fmt.Println("realEnthalpy: ", realEnthalpy)
+			// fmt.Println("almostEnthalpy: ", almostEnthalpy)
+			// fmt.Printf(
+			// 	"error: %.2f%%\n",
+			// 	math.Abs(float64(almostEnthalpy-realEnthalpy))/float64(almostEnthalpy)*100,
+			// )
+		}
+		// model.tick(r)
+		// m.logicalTick(r)
+
+		// tick the model
+		// m.enthalpicTick(r)
+		m.boltzmanTick(r)
+
+		m.time++
+		// tick(m, r)
+
+	}
+
+	// fmt.Println("we failed:", m.fails, "times")
+
+	return data
+
+}
+
+// ignore all these functions that r not rly used
+func run_simulation() stats.Series {
+	num_points := 100
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	series := make(stats.Series, 0, int(num_points))
+
+	// startTemp := 0.0001
+	// endTemp := 0.01
+	// // endTemp := 0.1
+	// dataPoints := 100
+
+	// for temp := startTemp; temp <= endTemp; temp += (endTemp - startTemp) / float64(dataPoints) {
+
+	for checkers := 1; checkers <= 50; checkers++ {
+
+		// for
+		// p := p / num_points
+
+		clear_line()
+		fmt.Print("this much done: ", checkers, "%")
+
+		model := init_model(SIZE, TICKS, num_points, r)
+
+		data := model.run_trial(r)
+
+		logged := logLog(data.toSeries())
+
+		_, slope, _ := LinearRegression(logged)
+
+		series = append(series, stats.Coordinate{X: float64(checkers), Y: slope})
+	}
+
+	// pretty_picture(model, "testing", 5)
+	return series
+
+}
+
+func testing() {
+
+	grid := gen_grid(3)
+
+	*grid.index(Point{x: 0, y: 0}) = 1
+	// fmt.Println(grid.index(Point{x: 0, y: 0}) == &grid[1][1])
+	// data = unordered_remove(data, 1)
+
+	clone := grid.clone()
+	*grid.index(Point{x: 0, y: 0}) = 2
+
+	fmt.Println(clone)
+}
+
+type DataPoint struct {
+	Time    int `json:"time"`
+	Bunnies int `json:"Bunnies"`
+	Foxes   int `json:"Foxes"`
+}
+
+type Data []DataPoint
+
+func (d Data) WriteToCSV(filename string) {
+	fmt.Println("wrtingt to this: ", filename)
+
+	var csv string
+
+	for _, point := range d {
+		csv += fmt.Sprintf("%d, %d, %d\n", point.Time, point.Bunnies, point.Foxes)
+	}
+
+	err := os.WriteFile(filename+".csv", []byte(csv), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func WriteToCSV(series stats.Series, filename string) {
+	fmt.Println("wrtingt to this: ", filename)
+
+	var csv string
+
+	for _, point := range series {
+		csv += fmt.Sprintf("%f, %f\n", point.X, point.Y)
+	}
+
+	err := os.WriteFile(filename+".csv", []byte(csv), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LinearRegression(s stats.Series) (float64, float64, error) {
+
+	if len(s) == 0 {
+		return 0, 0, nil
+	}
+
+	// Placeholder for the math to be done
+	var sum [5]float64
+
+	// Loop over data keeping index in place
+	i := 0
+	for ; i < len(s); i++ {
+		sum[0] += s[i].X
+		sum[1] += s[i].Y
+		sum[2] += s[i].X * s[i].X
+		sum[3] += s[i].X * s[i].Y
+		sum[4] += s[i].Y * s[i].Y
+	}
+
+	// Find gradient and intercept
+	f := float64(i)
+	gradient := (f*sum[3] - sum[0]*sum[1]) / (f*sum[2] - sum[0]*sum[0])
+	intercept := (sum[1] / f) - (gradient * sum[0] / f)
+
+	return intercept, gradient, nil
+}
+
+func logLog(series stats.Series) stats.Series {
+
+	logged := make(stats.Series, 0, len(series))
+
+	for _, point := range series {
+		logged = append(logged, stats.Coordinate{X: math.Log(point.X), Y: math.Log(point.Y)})
+	}
+
+	return logged
+}
+
+func (d *Data) toSeries() stats.Series {
+	series := make([]stats.Coordinate, 0, len(*d))
+
+	for _, point := range *d {
+		series = append(
+			series,
+			stats.Coordinate{X: float64(point.Time), Y: float64(point.Bunnies)},
+		)
+	}
+
+	return series
+}
+
+type Arguments struct {
+	file *string
+	// operation *string
+	chart  *string
+	output *string
+}
+
+func parse_args() Arguments {
+	// args := os.Args[1:]
+	// if len(args) >= 2 {
+	// 	if args[0] == "--file" || args[0] == "-f" {
+	// 		return Arguments{
+	// 			file:    args[1],
+	// 			is_file: true,
+	// 		}
+	// 	}
+	// }
+	//
+	// return Arguments{
+	// 	file:    "",
+	// 	is_file: false,
+	// }
+
+	args := Arguments{
+		file: flag.String("file", "", "path to data file"),
+		// operation: flag.String("op", "", "operation to perform"),
+		chart:  flag.String("chart", "", "type of chart to make"),
+		output: flag.String("out", "", "prefix of output files"),
+	}
+
+	flag.Parse()
+
+	return args
+}
+
+func main() {
+	// testing()
+	// return
+
+	var data Data
+
+	args := parse_args()
+
+	if *args.output == "" {
+		panic("no output file name specified")
+	}
+
+	// only run one_trial for testing purposes
+	// one_trial(*args.output)
+	// return
+
+	if file := *args.file; file != "" {
+		json_data, err := os.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+
+		json.Unmarshal(json_data, &data)
+
+	} else {
+
+		// data = run_simulation()
+		// data = one_trial()
+
+		data.WriteToCSV("data/" + *args.output)
+
+		// for _, d := range data {
+		// 	fmt.Println(d.X)
+		// }
+		//
+		// for _, d := range data {
+		// 	fmt.Println(d.Y)
+		// }
+		// fmt.Println(intercept, gradient)
+
+		// file, err := os.Create(*args.output + "data.json")
+		//
+		// if err != nil {
+		// 	panic(err)
+		// }
+		//
+		// defer file.Close()
+		//
+		// encoder := json.NewEncoder(file)
+		// encoder.SetIndent("", "  ")
+		// encoder.Encode(data)
+
+	}
+
+	// switch *args.chart {
+	//
+	// default:
+	// 	fmt.Println("uknown chart type: ", *args.chart)
+	// }
+
+}
+
+func Pointer[T any](t T) *T {
+	return &t
+}
+
+func find_max(data []opts.Chart3DData) float32 {
+	greatest := float64(0)
+	for _, value := range data {
+		v := value.Value[2]
+		switch v := v.(type) {
+		case float64:
+			if v > greatest {
+				greatest = v
+			}
+		default:
+			panic("howd that get in here")
+		}
+
+	}
+
+	return float32(greatest)
+}
+
+func make_3d_chart(data []opts.Chart3DData) {
+	surface := charts.NewSurface3D()
+
+	surface.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "Random Walker Data",
+		}),
+		charts.WithXAxis3DOpts(opts.XAxis3D{
+			Name: "Infection Chance (%)",
+		}),
+		charts.WithYAxis3DOpts(opts.YAxis3D{
+			Name: "# of Steps",
+		}),
+		charts.WithZAxis3DOpts(opts.ZAxis3D{
+			Name: "Average Spread Rate",
+		}),
+		charts.WithVisualMapOpts(opts.VisualMap{
+			Max:        find_max(data),
+			Min:        0,
+			Dimension:  "z",
+			Calculable: Pointer(true),
+			// Range:      []float32{1, 100},
+			InRange: &opts.VisualMapInRange{
+				Color: []string{"blue", "red"},
+			},
+		}),
+	)
+
+	surface.AddSeries("", data)
+	// SetSeriesOptions(
+	// 	charts.WithShading("realistic"),
+	// )
+
+	f, _ := os.Create("surface.html")
+	defer f.Close()
+	surface.Render(f)
+	err := exec.Command("xdg-open", "surface.html").Run()
+	if err != nil {
+		fmt.Println("couldnt open chart:", err)
+	}
+	exec.Command("hyprctl", "dispatch", "workspace", "2").Run()
+
+}
+
+// func cast_to_float(input []any) ([]float64, error) {
+// 	output := make([]float64, len(input))
+//
+// 	for i, elem := range input {
+// 		switch e := elem.(type) {
+// 		case float64:
+// 			output[i] = e
+//
+// 		default:
+// 			return []float64{}, fmt.Errorf("not of type float64")
+// 		}
+// 	}
+//
+// 	return output, nil
+// }
+
+func one_trial(filename string) {
+	// set all the parameters:
+
+	// calculate some values that make a nice video
+	frames := FPS * VID_TIME
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// make the model
+	model := init_model(SIZE, TICKS, frames, r)
+	// model := init_model(SIZE, TICKS, temp, useTable, 10, 100, r)
+
+	// run a trial
+	data := model.run_trial(r)
+
+	data.WriteToCSV("data/" + filename)
+
+	// for _, point := range data {
+	// 	fmt.Println(point.time)
+	// }
+	// fmt.Println()
+	// for _, point := range data {
+	// 	fmt.Println(point.enthalpy)
+	// }
+
+	// pretty_picture(model.grid, filename, true)
+	model.makeVid(FPS, filename)
+
+}
+
+func calc_color(percent float64) color.NRGBA {
+	RStart, REnd := 255.0, 1.0
+
+	// WOW !!! great code
+	return color.NRGBA{
+		R: uint8(RStart + (REnd-RStart)*percent),
+		A: 255,
+		// A: uint8(int(color_start.A) + round(float64(color_end.A-color_start.A)*percent)),
+	}
+
+}
+
+func pretty_picture(grid Grid, name string, save bool) *image.NRGBA {
+
+	size := len(grid)
+	screen_size := 960
+
+	scale := screen_size / size
+
+	// cropped := model.size - model.distance*2
+
+	img := image.NewNRGBA(image.Rect(0, 0, size*scale, size*scale))
+
+	// for y, row := range grid[model.distance : model.size-model.distance] {
+	// 	for x, value := range row[model.distance : model.size-model.distance] {
+	for y, row := range grid {
+		for x, value := range row {
+
+			var color color.Color = StateColor[value]
+
+			for i := range scale {
+				for j := range scale {
+					img.Set(x*scale+j, y*scale+i, color)
+				}
+			}
+		}
+	}
+
+	if save {
+		f, err := os.Create(name + ".png")
+		if err != nil {
+			// log.Fatal(err)
+		}
+
+		if err := png.Encode(f, img); err != nil {
+			f.Close()
+			// log.Fatal(err)
+		}
+
+		if err := f.Close(); err != nil {
+			// log.Fatal(err)
+		}
+	}
+
+	return img
+
+	// file, err := os.Create(name + "-data.png")
+	//
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// defer file.Close()
+	//
+	// encoder := json.NewEncoder(file)
+	// encoder.SetIndent("", "  ")
+	// // encoder.Encode(model)
+	// encoder.Encode(struct {
+	// 	P          float64 `json:"p"`
+	// 	Size       int     `json:"size"`
+	// 	Infected   int     `json:"infected"`
+	// 	Population int     `json:"population"`
+	// }{
+	// 	model.p,
+	// 	model.size,
+	// 	model.infected,
+	// 	model.people,
+	// })
+
+	// exec.Command("chafa", "image.png").Run()
+}
+
+func (m *Model) makeVid(fps int, name string) {
+
+	// start := time.Now()
+
+	first := pretty_picture(m.grids[0], "", false)
+
+	bounds := first.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+
+	// options := vidio.Options{FPS: float64(fps), Loop: 0, Delay: 1000}
+	options := vidio.Options{FPS: float64(fps)}
+	gif, _ := vidio.NewVideoWriter(name+".mp4", w, h, &options)
+	defer gif.Close()
+
+	// prettyTime := 0.0
+
+	for i := range m.grids {
+
+		// prettyStart := time.Now()
+		img := pretty_picture(m.grids[i], name+"-"+fmt.Sprintf("%d", i), false).Pix
+		// prettyTime += time.Since(prettyStart).Seconds()
+		gif.Write(img)
+	}
+
+	// elapsedTime := time.Since(start)
+	// fmt.Printf("elapsed time: %.2f\n", elapsedTime.Seconds())
+	// fmt.Println(prettyTime)
+}
+
+// func (m *Model)
+
+// func make_graph(grid Grid) {
+// 	chart := charts.NewHeatMap()
+// 	// set some global options like Title/Legend/ToolTip or anything else
+// 	chart.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+// 		Title:    "My first bar chart generated by go-echarts",
+// 		Subtitle: "It's extremely easy to use, right?",
+// 	}))
+//
+// 	// Put data into instance
+// 	chart.SetXAxis([]string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"})
+// 	// Where the magic happens
+// 	f, _ := os.Create("chart.html")
+// 	chart.Render(f)
+// }
